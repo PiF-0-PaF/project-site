085cbeb71ac1f984faf7b197ee11c6951b66076a
unknown <ianvinokurow@gmail.com>
2023-04-28 23:13:10 +0500

    save

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="App"><Main_page/></div>
+</template>
+
+<script>
+import Main_page from '@/components/Main_page'
+
+export default {
+  name: 'App',
+  components: {
+    Main_page,
+  }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
